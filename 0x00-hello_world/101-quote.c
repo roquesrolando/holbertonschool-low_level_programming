9e6837b12c513e7bab965e078b9b639e2a462085
roquesrolando <roquesrolando@hotmail.com>
2021-02-03 20:39:21 +0000

    101-quote.c answer

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 /**
 * main - Writes the required quote
-* Return: 0
+* Return: 1
 */
 
 int main(void)
 {
-fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
-return (1);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
