db255858293c6e14f1c7f8108de3e1aeca330394
roquesrolando <roquesrolando@hotmail.com>
2021-02-03 20:14:45 +0000

    101-quote.c answer

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - function prints the required text and returns 0
-* Return: 0
+* main - function prints the required text and returns 1
+* Return: 1
 */
 int main(void)
 {
 fprintf(stderr, "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (0);
+return (1);
 }
