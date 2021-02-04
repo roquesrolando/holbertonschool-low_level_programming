bb62a296436d97930c91e1ec148f1859cd419f7c
roquesrolando <roquesrolando@hotmail.com>
2021-02-04 19:57:58 +0000

    9-print_comb.c answer

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - function prints the required text and returns 0
+* Return: 0
+*/
+int main(void)
+{
+	int a;
+	for(a='0'; a <= '9'; a++)
+{
+	putchar(a);
+	if (a <= '8')
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+	putchar ('\n');
+	return (0);
+}
