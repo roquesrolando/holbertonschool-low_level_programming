214a12b0153b3e0868d8bff53012b278105871fa
roquesrolando <roquesrolando@hotmail.com>
2021-02-04 18:39:38 +0000

    6-print_numberz.c answer

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - function prints the required text and returns 0
+* Return: 0
+*/
+int main(void)
+{
+	int a;
+
+	for (a = '0' ; a <= '9' ; a++)
+{
+	putchar(a);
+}
+	putchar ('\n');
+	return (0);
+}
