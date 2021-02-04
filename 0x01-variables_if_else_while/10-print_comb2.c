2a697efd86e3442c65b647662fdba093c8fb01ea
roquesrolando <roquesrolando@hotmail.com>
2021-02-04 20:14:18 +0000

    10-print_comb2.c answer

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - function prints the required text and returns 0
+* Return: 0
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 100 ; a++)
+{
+	putchar((a / 10) + '0');
+	putchar((a % 10) + '0');
+}
+	if (a <= 99)
+{
+	putchar(',');
+	putchar(' ');
+}
+	putchar ('\n');
+	return (0);
+}
