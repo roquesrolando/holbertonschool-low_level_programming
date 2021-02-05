0d6fd47421199c2cd6168789cc72f26105ef18a4
roquesrolando <roquesrolando@hotmail.com>
2021-02-05 00:53:40 +0000

    100-print_comb3.c answer

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - function prints the required text and returns 0
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = a + 1; b <= '9'; + b++)
+		{
+			putchar(a);
+			putchar(b);
+			if (a != '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
