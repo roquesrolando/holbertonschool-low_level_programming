925d71b6bbac1c9a46b09a30fcdbdc1e29542e36
roquesrolando <roquesrolando@hotmail.com>
2021-02-05 01:23:29 +0000

    101-print_comb4.c answer

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - function prints the required text and returns 0
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = a + 1; b <= '9'; b++)
+		{
+			for (c = b + 1; c <= '9'; c++)
+			{
+			putchar(a);
+			putchar(b);
+			putchar(c);
+			}
+			if (a != '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
