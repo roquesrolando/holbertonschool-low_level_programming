a9d26279fc99ce97ca072ea4c3eb9d474f6b8dda
roquesrolando <roquesrolando@hotmail.com>
2021-02-09 19:47:55 +0000

    update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,1 +1,49 @@
+#include "holberton.h"
+/**
+ * times_table - function prints multiples of 9
+ *
+ * @r: valued being sent
+ */
 
+void print_times_tablet(int n)
+{
+	int num;
+	int multi;
+	int total;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+
+			for (multi = 1; multi <= n; multi++)
+			{
+				_putchar(44);
+				_putchar(32);
+
+				total = num * multi;
+
+				if (total <= 99)
+					_putchar(32);
+
+				if (total <= 9)
+
+					_putchar(32);
+
+				if (total >= 100)
+				{
+					_putchar((total / 100) + '0');
+					_putchar(((total / 10)) % 10 + '0');
+				}
+				else if (total <= 99 && total >= 10)
+				{
+					_putchar((total / 10) + '0');
+				}
+				_putchar((total % 10) + '0');
+			}
+
+			_putchar('\n');
+		}
+	}
+}
