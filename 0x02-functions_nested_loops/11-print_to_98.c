0f1c1bd732481b7395c4d5648c14868fccc23450
roquesrolando <roquesrolando@hotmail.com>
2021-02-09 05:02:15 +0000

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,34 +2,8 @@
 #include <unistd.h>
 #include <stdio.h>
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_to_98(0);
-    print_to_98(98);
-    print_to_98(111);
-    print_to_98(81);
-    print_to_98(-10);
-    return (0);
-}
-
-/**
  * print_to_98 - print all number
- * start: starting point
+ * @start: starting point
  * Return: 0
  */
 
