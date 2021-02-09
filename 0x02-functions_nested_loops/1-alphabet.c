4608dc850b3c3b3e4fc53d699c01908881e3b529
roquesrolando <roquesrolando@hotmail.com>
2021-02-09 03:16:34 +0000

    1-alphabet.c answer

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,25 +1,8 @@
-#include <stdio.h>
-#include <unistd.h>
 #include "holberton.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * print_alphabet - prints alphabet
+ * Return: 0
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-void print_alphabet(void);
-
-	int main(void)
-	{
-		print_alphabet();
-		return (0);
-	}
-
 	void print_alphabet(void)
 	{
 		char lowercaseletter;
