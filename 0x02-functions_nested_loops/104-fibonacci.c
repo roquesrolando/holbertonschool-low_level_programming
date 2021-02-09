f39cd8e96d86fb9c6d03078d4b6191bbc14aff73
roquesrolando <roquesrolando@hotmail.com>
2021-02-09 23:49:46 +0000

    update

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - prints fibonacci numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	unsigned long fib1 = 1, fib2 = 2, fib3, num, count = 0;
+
+	num = 100;
+
+	printf("%lu, ", fib1);
+
+	printf("%lu, ", fib2);
+
+	count = 4;
+
+	while (count < num)
+
+	{
+		fib3 = fib1 + fib2;
+
+		count++;
+
+		printf("%lu", fib3);
+
+		fib1 = fib2;
+
+		fib2 = fib3;
+		if (count != 100)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
+
