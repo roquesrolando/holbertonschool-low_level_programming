1b4738b4085224e550ed8f47d01a48742ae4668e
roquesrolando <roquesrolando@hotmail.com>
2021-02-12 00:20:14 +0000

    update

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * print_number - print interger
+ * @n: the number
+ */
+
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		num = -num;
+		_putchar('-');
+		_putchar(9 + '0');
+		_putchar(8 + '0');
+	}
+	else if (num == 0)
+	{
+		_putchar(num + '0');
+	}
+	else if (num <= 99)
+	{
+		_putchar((num / 10) + '0');
+		_putchar((num % 10) + '0');
+	}
+	else if (num <= 999)
+	{
+		_putchar(((num / 10) / 10) + '0');
+		_putchar(((num / 10) % 10) + '0');
+		_putchar((num % 10) + '0');
+	}
+	else if (num >= 999)
+	{
+		_putchar((((num / 10) / 10) / 10) + '0');
+		_putchar((((num / 10) / 10) % 10) + '0');
+		_putchar(((num / 10) % 10) + '0');
+		_putchar((num % 10) + '0');
+	}
+}
