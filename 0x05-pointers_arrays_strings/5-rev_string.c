0e9ddc2e5547eda3f60d05aa83782c334a18289b
roquesrolando <roquesrolando@hotmail.com>
2021-02-15 04:17:55 +0000

    update

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * rev_string - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
@@ -12,6 +12,7 @@ void rev_string(char *s)
 	char sub;
 
 	for (length = 0; s[length] != '\0'; length++)
+		;
 
 	reverse = length - 1;
 	{
