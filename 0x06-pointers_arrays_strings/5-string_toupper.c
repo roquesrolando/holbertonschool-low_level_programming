3c682e344c883dd63d7c79c5d4ddb437032885d8
roquesrolando <roquesrolando@hotmail.com>
2021-02-18 20:59:08 +0000

    update

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - change lowercase
+ * @s: string
+ *
+ * Return: changed string
+ */
+
+char *string_toupper(char *a)
+{
+	int i = 0;
+
+	while (a[i])
+	{
+		if (a[i] >= 97 && a[i] <= 122)
+			a[i] -= 32;
+		i++;
+	}
+	return (a);
+}
