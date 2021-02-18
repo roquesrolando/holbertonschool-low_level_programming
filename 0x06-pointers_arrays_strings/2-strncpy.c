1fa4487459a8b0dd385265561a53c18d81bd8bc0
roquesrolando <roquesrolando@hotmail.com>
2021-02-18 18:04:15 +0000

    update

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,15 +11,16 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i != n; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest[i] = src[i];
-
-		if (src[i] == '\0')
+		if (i < n)
 		{
-			dest[i] = '\0';
-			break;
+			dest[i] = src[i];
 		}
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
 	}
 	return (dest);
 }
