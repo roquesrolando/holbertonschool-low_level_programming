0261e24b7fa334a808c608e3c9f36d06386bc693
roquesrolando <roquesrolando@hotmail.com>
2021-02-18 19:34:48 +0000

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,7 +7,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int n;
+	int i;
 
-	return (n = *s1 - *s2);
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
 }
