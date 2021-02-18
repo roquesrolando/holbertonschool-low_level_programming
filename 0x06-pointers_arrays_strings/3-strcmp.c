7b1cce61fb5d2e1d54be61c4dcedfc2fde63c82f
roquesrolando <roquesrolando@hotmail.com>
2021-02-18 19:12:57 +0000

    update

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _strcmp - compares pointers
+ * @s1: 1st string
+ * @s2: 2nd string
+ * Return: the difference
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int n;
+
+	return (n = *s1 - *s2);
+}
