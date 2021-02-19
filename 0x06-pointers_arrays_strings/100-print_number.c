72fe22c7079cf93a058b1c4effee9fe9f41188fa
roquesrolando <roquesrolando@hotmail.com>
2021-02-19 03:32:29 +0000

    update

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,7 +1,26 @@
-#include "holberton"
+#include "holberton.h"
+
 /**
-* print_number - prints integer
-*
-*
-*/
-void print_number(int n);
+ * print_number - print interger
+ * @n: the number
+ */
+
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		num = -n;
+		_putchar('-');
+	}
+	else
+	{
+		num = n;
+	}
+	if (num / 10)
+	{
+		print_number(num / 10);
+	}
+	_putchar(num % 10 + '0');
+}
