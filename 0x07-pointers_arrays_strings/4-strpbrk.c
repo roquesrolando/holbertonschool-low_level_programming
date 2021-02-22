85614b31be1c6017f29a69f89a7b77493997b623
roquesrolando <roquesrolando@hotmail.com>
2021-02-22 18:58:40 +0000

    update

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - searches a string for bytes
+ * @s: the string
+ * @accept: the bytes it searches
+ *
+ * Return: the bytes found
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	for( ; *s; s++)
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+			{
+				return(s);
+			}
+		}
+	}
+	return('\0');
+}
