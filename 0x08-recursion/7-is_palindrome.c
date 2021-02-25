235ba56a537f32545394138b9c03bd4a11c21306
roquesrolando <roquesrolando@hotmail.com>
2021-02-25 19:36:50 +0000

    update

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,56 +1,54 @@
 #include "holberton.h"
+
 /**
- * palindrome_check - check for palindrome
- * @s: reverse string
- * @i: normal string
+ * check_palindrome - check for palindrome
+ * @str: string
+ * @idx: starting point
+ * @len: length of string
  *
  * Return: 1 if it's palindrome 0 if it's not
  */
-
-int palindrome_check(char *s, char *i)
+int check_palindrome(char *str, int idx, int len)
 {
-	if (*s != *i)
-		return (0);
-
-	if (*s == *i)
-		palindrome_check(s - 1, i + 1);
-	return (1);
-
-}
-/**
- * palindrome_mover - reverses string
- * @s: reverse string
- * @i: normal string
- *
- * Return: 1 if it's palindrome 0 if it's not
- */
-
-int palindrome_mover(char *s, char *i)
-{
-	int count = 0;
-
-	if (*s == '\0')
-		palindrome_mover(s + 1, i);
-
-	count = count + palindrome_check(s - 1, i);
-	return (count);
-}
-/**
- * is_palindrome - check for palindrome
- * @s: reverse string
- *
- * Return: 1 if it's palindrome 0 if it's not
- */
-
-
-int is_palindrome(char *s)
-{
-	char *i;
-
-	i = s;
-
-	if (*s == '\0')
+	if (idx == len)
 		return (1);
 
-	return (palindrome_mover(s, i));
+	if (str[idx] != str[len - (idx + 1)])
+		return (0);
+
+	return (check_palindrome(str, idx + 1, len));
+}
+
+/**
+ * _strlen - gets length of string
+ * @str: string
+ *
+ * Return: 1 if it's palindrome 0 if it's not
+ */
+int _strlen(char *str)
+{
+	int len;
+
+	if (*str == '\0')
+		return (0);
+
+	len = _strlen(str + 1);
+	return (len + 1);
+}
+
+/**
+ * is_palindrome - check for palindrome
+ * @str: reverse string
+ *
+ * Return: 1 if it's palindrome 0 if it's not
+ */
+int is_palindrome(char *str)
+{
+	int len = _strlen(str);
+	int idx = 0;
+
+	if (*str == '\0')
+		return (1);
+
+	return (check_palindrome(str, idx, len));
 }
