4e03e114e34c5f45924895bd4bc8d7e2608b5b21
roquesrolando <roquesrolando@hotmail.com>
2021-02-27 15:18:55 +0000

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - prints mynameis
+ * @argc: count count
+ * @argv: vector
+ *
+ * Return: 0
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
