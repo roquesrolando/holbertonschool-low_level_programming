f65d121bbe28a42523f537aa4659fa3b976257ac
roquesrolando <roquesrolando@hotmail.com>
2021-03-02 23:30:58 +0000

    update

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * **alloc_grind - creates pointer to 2D array
+ * @width: wide of array
+ * @height: height of array
+ *
+ * Return: pointers for 2D
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, count = 0;
+	int **s;
+
+	s = (int **)malloc(height * sizeof(int *));
+
+	for (i = 0; i < height; i++)
+	{
+		s[i] = (int *)malloc(width * sizeof(int));
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			s[i][j] = count;
+	}
+	return (s);
+}
