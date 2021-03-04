407fc3210378802d849a8964c1afecfbdf073cc4
roquesrolando <roquesrolando@hotmail.com>
2021-03-04 16:23:03 +0000

    update

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - it checks if malloc allocated memory
+ * @b: size 
+ *
+ * Return: void pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *s;
+
+	s = malloc(b);
+
+	if (s == 0)
+	{
+		exit(98);
+	}
+	return (s);
+}
