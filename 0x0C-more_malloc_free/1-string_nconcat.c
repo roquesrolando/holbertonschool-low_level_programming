f9349bb6143153dd07a3a4995ab595a730690837
roquesrolando <roquesrolando@hotmail.com>
2021-03-04 17:06:39 +0000

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,17 +17,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return ("");
 	if (s2 == 0)
 		return ("");
+
 	for (count = 0; s1[count] != '\0'; count++)
 		;
 	for (count2 = 0; s2[count2] != '\0'; count2++)
 		;
+
 	if (n >= count2)
 		sum = count + count2;
 	else
 		sum = count + n;
+	
 	s = malloc(sizeof(char) * (sum + 1));
+
 	if (s == 0)
 		return (NULL);
+
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		s[i] = s1[i];
