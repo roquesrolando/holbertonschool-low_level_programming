aef7de6a954453890cd7f00ef57baef37251ffd3
roquesrolando <roquesrolando@hotmail.com>
2021-03-04 17:02:17 +0000

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- *
- *
- *
- *
+ * *string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes
+ * Return: concatenated string
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -16,31 +17,36 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return ("");
 	if (s2 == 0)
 		return ("");
-
 	for (count = 0; s1[count] != '\0'; count++)
 		;
-
 	for (count2 = 0; s2[count2] != '\0'; count2++)
 		;
-
 	if (n >= count2)
 		sum = count + count2;
 	else
 		sum = count + n;
-
 	s = malloc(sizeof(int) * (sum + 1));
-
 	if (s == 0)
 		return (NULL);
-
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		s[i] = s1[i];
 	}
-	for (j = 0; j < n ; j++)
+	if (n < count2)
 	{
-		s[i] = s2[j];
-		i++;
+		for (j = 0; j < n ; j++)
+		{
+			s[i] = s2[j];
+			i++;
+		}
+	}
+	if (n >= count2)
+	{
+		for (j = 0; s2[j] != '\0'; j++)
+		{
+			s[i] = s2[j];
+			i++;
+		}
 	}
 	return (s);
 }
