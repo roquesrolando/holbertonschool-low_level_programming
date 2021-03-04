ad1bc9f4ae4d798b52e068cbd5359133bbeb739b
roquesrolando <roquesrolando@hotmail.com>
2021-03-04 23:13:46 +0000

    update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,10 +26,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (new_size == 0 && ptr != 0)
 	{
-		free (ptr);
+		free(ptr);
 		return (NULL);
 	}
 	s = malloc(new_size);
-	free (ptr);
+	free(ptr);
 	return (s);
 }
