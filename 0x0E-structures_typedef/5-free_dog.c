bdb1b365330e4a818e01ab016bf386478ac05f34
roquesrolando <roquesrolando@hotmail.com>
2021-03-08 21:56:27 +0000

    update

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -4,10 +4,12 @@
  * free_dog - free new dog
  * @d: new dog
  */
-void free_dog(dog_t *d)
+
+void free_dog(dog_t *scoobs)
 {
 	if (scoobs == NULL)
 		return;
+
 	free((*scoobs).name);
 	free((*scoobs).owner);
 	free(scoobs);
