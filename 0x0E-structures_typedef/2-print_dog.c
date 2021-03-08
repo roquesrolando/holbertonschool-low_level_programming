92e1440ae3df8ff058b3476efab3a1d83e33883f
roquesrolando <roquesrolando@hotmail.com>
2021-03-08 19:02:32 +0000

    update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,7 +18,7 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: %s\n", (*d).name);
 
-		if ((*d).age == 0)
+		if ((*d).age == 0.000000)
 			printf("Age: (nil)\n");
 		else
 			printf("Age: %f\n", (*d).age);
