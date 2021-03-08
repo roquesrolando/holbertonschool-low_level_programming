31dca52d54805f7da4eeb5bcd0dae58a621e6c4c
roquesrolando <roquesrolando@hotmail.com>
2021-03-08 20:55:10 +0000

    update

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,76 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+/**
+ * *new_dog - create a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ *
+ * Return: new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *scoobs;
+	char *new_name;
+	char *new_owner;
+
+	if (name == 0)
+		return (NULL);
+	if (owner == 0)
+		return (NULL);
+
+	new_name = malloc(sizeof(_strlen(name)) + 1);
+	new_owner = malloc(sizeof(_strlen(owner)) + 1);
+
+	scoobs = malloc(sizeof(struct dog));
+	(*scoobs).name = name;
+	(*scoobs).age = age;
+	(*scoobs).owner = owner;
+
+	new_name = _strcpy(new_name, name);
+	new_owner = _strcpy(new_owner, owner);
+
+	return (scoobs);
+}
+/**
+ * _strcpy - Copy a string
+ * @dest: Destination value
+ * @src: Source value
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i++] = '\0';
+
+	return (dest);
+}
+/**
+ * _strlen - Returns the length of a string
+ * @s: String to count
+ *
+ * Return: String length
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+
+	return (c);
+}
