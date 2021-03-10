81febc0c4d8bfcd4398204df3298d36d95a92290
roquesrolando <roquesrolando@hotmail.com>
2021-03-10 17:12:54 +0000

    update

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,9 +14,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (array == 0)
-		return;
+		return (-1);
 	if (cmp == 0)
-		return;
+		return (-1);
 	if (size <= 0)
 		return (-1);
 
