9561d9fa4d37ede51ffde4b831115dafb8fe0d17
roquesrolando <roquesrolando@hotmail.com>
2021-03-10 17:06:46 +0000

    update

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,6 +11,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
+	if (array == 0)
+		return;
+	if (action == 0)
+		return;
+
 	for (i = 0; i < size; i++)
 		action(array[i]);
 }
