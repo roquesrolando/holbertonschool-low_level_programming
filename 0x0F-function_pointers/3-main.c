d2ae458339838e4857d1a2c2d7a8ab259fa9951a
roquesrolando <roquesrolando@hotmail.com>
2021-03-10 20:03:31 +0000

    update

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+
+/**
+ *
+ *
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int (*num)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num = get_op_func(argv[2]);
+
+	if (num == 0)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", num(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum a and b
+ *
+ * Return: total
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference of a and b
+ *
+ * Return: total
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication of a and b
+ *
+ * Return: total
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of a and b
+ *
+ * Return: total
+ */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder of division of a and b
+ *
+ * Return: total
+ */
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
