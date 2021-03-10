2862a2a4d9977b8689d8cd001189f462a8767ebd
roquesrolando <roquesrolando@hotmail.com>
2021-03-10 16:55:22 +0000

    update

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - runs the array
+ * @array: the array
+ * @size: size of the array
+ * @action: pointer to a function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
