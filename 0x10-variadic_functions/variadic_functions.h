2285e62725c79069e2c06ecdac76893202a49e52
roquesrolando <roquesrolando@hotmail.com>
2021-03-11 21:34:40 +0000

    more prototype

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,9 +2,20 @@
 #define HEADER
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list ap);
+void print_int(va_list ap);
+void print_float(va_list ap);
+void print_string(va_list ap);
+
+typedef struct function
+{
+	char function;
+	void (*f)(va_list ap);
+} f_t;
 #endif
