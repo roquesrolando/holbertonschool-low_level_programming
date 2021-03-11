b22144b4ab37e124d2021dc107e67793c679a6f9
roquesrolando <roquesrolando@hotmail.com>
2021-03-11 16:02:45 +0000

    update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,10 @@
+#ifndef HEADER
+#define HEADER
+#include <stdarg.h>
+#include <stdio.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+#endif
