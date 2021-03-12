72b2873292ed36759b5c833cc0fac3eced5d0b8e
roquesrolando <roquesrolando@hotmail.com>
2021-03-12 18:48:57 +0000

    update

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print opcodes
+ * @argc: count
+ * @argv: vector
+ *
+ * Return: nothing
+ */
+
+int main(int argc, char *argv[])
+{
+	char *code;
+	int i, bts;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	code = (char *)&main;
+	bts = atoi(argv[1]);
+
+	if (bts < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < bts; i++)
+	{
+		printf("%02hhx ", code[i]);
+	}
+	printf("%02hhx\n", code[i]);
+	return (0);
+}
