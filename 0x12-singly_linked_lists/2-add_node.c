eff71a8f3bace8409d40693ea619fc76f23335d4
roquesrolando <roquesrolando@hotmail.com>
2021-03-18 19:42:46 +0000

    uploaded task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * add_node - add nodes to link list
+ * @head: the address
+ * @str: the string
+ *
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new-> str = strdup(str);
+	new-> len = _strlen(str);
+	new-> next = *head;
+	*head = new;
+
+	return (new);
+}
+
+/**
+ * _strlen - returns lenght of string
+ * @str: string
+ *
+ * Return: string lenght
+ */
+
+int _strlen(const char *str)
+{
+	int i = 0;
+	int count = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
+}
