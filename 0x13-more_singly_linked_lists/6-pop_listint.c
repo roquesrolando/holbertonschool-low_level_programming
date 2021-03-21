4263a8c600960daedc2c6bdb5af0a1cead7d85d5
roquesrolando <roquesrolando@hotmail.com>
2021-03-21 16:44:27 +0000

    uploaded task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node
+ * @head: pointer to head node
+ *
+ * Return: returns head node data
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *hold;
+	int data;
+
+	if (head == NULL)
+		return (0);
+
+	while (*head != NULL)
+	{
+		data = (*head)->n;
+		hold = *head;
+		*head = (*head)->next;
+		free(hold);
+		break;
+	}
+	return (data);
+}
