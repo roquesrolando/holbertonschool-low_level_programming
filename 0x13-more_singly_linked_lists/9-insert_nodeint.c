f6a55151e13a30423ea68c3fc04d65bdedc454f4
roquesrolando <roquesrolando@hotmail.com>
2021-03-21 18:20:10 +0000

    betty fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,10 +1,12 @@
 #include "lists.h"
 
 /**
+ * insert_nodeint_at_index - insert a node
+ * @head: the linked list
+ * @idx: index to insert node
+ * @n: data to insert
  *
- *
- *
- *
+ * Return: new node
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
