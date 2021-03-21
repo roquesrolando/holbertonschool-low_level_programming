80ab327632e4ba438fad8b7c7cb2edb9cf145509
roquesrolando <roquesrolando@hotmail.com>
2021-03-21 18:36:23 +0000

    update fixes

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,6 +24,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 
 	hold = *head;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	while (hold)
 	{
 		if (count == idx - 1)
