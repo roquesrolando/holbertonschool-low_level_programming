5b9783f423c6356d272e5c1096790f01bb652126
roquesrolando <roquesrolando@hotmail.com>
2021-03-22 21:15:16 +0000

    uploaded task 12

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints linked list
+ * @head: linked list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *array[1024];
+	int j = 0, i = 0, nodes = 0;
+
+	while (head)
+	{
+		for (j = 0; j < i; j++)
+		{
+			if (array[j] == head)
+				exit(98);
+		}
+		array[i] = head;
+		printf("[%p] %i\n", (void *)head, head->n);
+		head = head->next;
+		nodes++;
+		i++;
+	}
+	return (nodes);
+}
