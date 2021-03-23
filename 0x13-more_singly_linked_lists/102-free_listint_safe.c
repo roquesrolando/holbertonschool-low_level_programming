5560af366710dbc644114d152d82b65c952897f7
roquesrolando <roquesrolando@hotmail.com>
2021-03-23 20:38:39 +0000

    updated fixes for 102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -12,15 +12,18 @@ size_t free_listint_safe(listint_t **h)
 	listint_t *hold, *array[1024];
 	int count = 0, i = 0, j = 0;
 
-	while (*h)
+	while (*h != NULL)
 	{
 		for (j = 0; j < i; j++)
 		{
 			if (array[j] == *h)
 			{
-				(*h)->next = NULL;
+				*h = NULL;
+				count++;
+				return (count);
 			}
 		}
+		array[i] = *h;
 		hold = *h;
 		*h = (*h)->next;
 		free(hold);
