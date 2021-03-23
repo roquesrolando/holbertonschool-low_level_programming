8f2c71edb055e085f462f51f3a58f3a90dc0979f
roquesrolando <roquesrolando@hotmail.com>
2021-03-23 20:25:51 +0000

    uploaded task 13 and 14

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - Frees a linked list
+ * @h: The head
+ *
+ * Return: Nothing
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *hold, *array[1024];
+	int count = 0, i = 0, j = 0;
+
+	while (*h)
+	{
+		for (j = 0; j < i; j++)
+		{
+			if (array[j] == *h)
+			{
+				(*h)->next = NULL;
+			}
+		}
+		hold = *h;
+		*h = (*h)->next;
+		free(hold);
+		count++;
+		i++;
+	}
+	free(*h);
+	*h = NULL;
+	count++;
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - find the loop
+ * @head: linked list
+ *
+ * Return: number of nodes
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	const listint_t *array[1024];
+	int j = 0, i = 0, nodes = 0;
+
+	while (head)
+	{
+		for (j = 0; j < i; j++)
+		{
+			if (array[j] == head)
+			{
+				printf("-> [%p] %i\n", (void *)head, head->n);
+				return (head);
+			}
+		}
+		array[i] = head;
+		head = head->next;
+		nodes++;
+		i++;
+	}
+	return (NULL);
+}
