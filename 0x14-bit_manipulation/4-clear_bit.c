b27041758e8ef9497f4a5d8c5abf8a3ca8710426
roquesrolando <roquesrolando@hotmail.com>
2021-03-25 21:39:31 +0000

    upload task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - set value to 1 on index
+ * @n: number
+ * @index: index of the number
+ *
+ * Return: 1 if it worked
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int set;
+
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+
+	set = ~(1 << index);
+	*n = *n & set;
+	return (1);
+}
