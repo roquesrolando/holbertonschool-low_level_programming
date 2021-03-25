70fa08c98194e1207bd32908dfe30851d6379f6b
roquesrolando <roquesrolando@hotmail.com>
2021-03-25 21:19:51 +0000

    uploaded task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * set_bit - set value to 1 on index
+ * @n: number
+ * @index: index of the number
+ *
+ * Return: 1 if it worked
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	long unsigned int set;
+
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+
+	set = 1 << index;
+	*n = *n | set;
+	return (1);
+}
