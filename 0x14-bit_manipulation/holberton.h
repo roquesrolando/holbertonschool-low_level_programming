a5d12d47ef5443180c1128e00e574ea066855e25
roquesrolando <roquesrolando@hotmail.com>
2021-03-25 16:50:59 +0000

    fixes

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,9 +1,7 @@
-#ifndef HOLBERTON
-#define HOLBERTON
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
