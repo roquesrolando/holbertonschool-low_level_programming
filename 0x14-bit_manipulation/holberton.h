4ebacd89b1b353779d146691c0bce494f59811f5
roquesrolando <roquesrolando@hotmail.com>
2021-03-25 19:39:46 +0000

    update

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -2,6 +2,7 @@
 #define HOLBERTON_H
 
 #include <stdlib.h>
+#include <stdio.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -10,5 +11,6 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int _strlen(unsigned long int n);
 
 #endif
