9327916014effa01a4f700ddab7eaa1c7f040cab
roquesrolando <roquesrolando@hotmail.com>
2021-03-25 16:35:15 +0000

    upload task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int total = 0, bit = 0, exponent = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+		if (b[i] == '1')
+			bit = 1;
+		else if (b[i] == '0')
+			bit = 0;
+
+		total += bit << exponent;
+		exponent++;
+	}
+	return (total);
+}
