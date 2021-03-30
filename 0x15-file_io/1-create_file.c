8b57fc15b6513b54598da07ebc25a62bae33c052
roquesrolando <roquesrolando@hotmail.com>
2021-03-30 16:45:16 +0000

    uploaded task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+  * create_file - creates a file
+  * @filename: the file
+  * @text_content: the text
+  *
+  * Return: 1 if created`
+  */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
