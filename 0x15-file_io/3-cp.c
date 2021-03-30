799d8344a5aaedb9fcf35bc7819b09e6d11a1ad1
roquesrolando <roquesrolando@hotmail.com>
2021-03-30 19:49:56 +0000

    update fixes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * main - copy file to another file
  * @argc: count
@@ -7,7 +6,6 @@
  *
  * Return:..
  */
-
 int main(int argc, char *argv[])
 {
 	char buff[1024];
@@ -24,28 +22,29 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
+	scan = read(from, buff, 1024);
+	close(from);
+	if (from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", from);
+		exit(100);
+	}
 	to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read to %s\n", argv[2]);
 		exit(98);
 	}
-	scan = read(from, buff, 1024);
-
 	written = write(to, buff, scan);
 	if (written == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	if (close(to) == -1)
+	close(to);
+	if (to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", to);
-		exit(100);
-	}
-	if (close(from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", from);
 		exit(100);
 	}
 	return (0);
