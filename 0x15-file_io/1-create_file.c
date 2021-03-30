5f1a88e9906bf0943b3b5bdbcea160daddc1ff37
roquesrolando <roquesrolando@hotmail.com>
2021-03-30 17:04:07 +0000

    update fixes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,6 +13,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	if (!text_content)
+		return (-1);
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
