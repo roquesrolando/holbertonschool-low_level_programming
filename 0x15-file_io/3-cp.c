47c0a9aeafc5ba754831090b27bf7598e1f1e0d3
roquesrolando <roquesrolando@hotmail.com>
2021-03-30 21:33:39 +0000

    fix error messages

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -40,6 +40,11 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
+	if (scan == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	if (close(from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", from);
