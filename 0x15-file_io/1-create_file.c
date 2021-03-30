297b2eae4b54fc60ef7178bebed9560d1ee84439
roquesrolando <roquesrolando@hotmail.com>
2021-03-30 17:07:35 +0000

    update fixes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,8 +13,6 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (!text_content)
-		return (-1);
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
@@ -25,7 +23,8 @@ int create_file(const char *filename, char *text_content)
 		i++;
 		count++;
 	}
-	written = write(fd, text_content, count);
+	if (text_content)
+		written = write(fd, text_content, count);
 	if (written == -1)
 		return (-1);
 
