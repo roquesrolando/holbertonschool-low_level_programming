6bd68045107eec0ad03670f08d716f2db9ade7d0
roquesrolando <roquesrolando@hotmail.com>
2021-05-02 15:10:47 +0000

    fix output

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -23,7 +23,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (*head)
 	{
+		new->prev = (*head)->prev;
 		(*head)->prev = new;
+		*head = new
 	}
-	return (new);
+	return (*head);
 }
