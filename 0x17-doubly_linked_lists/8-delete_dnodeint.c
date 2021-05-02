47bdfde082e7ef22d8c26a4c59a9a333926fc590
roquesrolando <roquesrolando@hotmail.com>
2021-05-02 16:10:50 +0000

    first commit

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - delete a node
+ * @head: the linked list
+ * @idx: index to delete node
+ *
+ * Return: 1 if succeded or -1 if failed
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *hold, *new;
+	unsigned int count = 0;
+
+	if (head == NULL)
+		return (-1);
+
+	hold = *head;
+	if (index == 0)
+	{
+		if (*head == NULL)
+		{
+			return (-1);
+		}
+		*head = (*head)->next;
+		free(hold);
+		return (1);
+	}
+	while (hold)
+	{
+		if (count == index - 1)
+		{
+			new = hold->next;
+			hold->next = new->next;
+			free(new);
+			return (1);
+		}
+		hold = hold->next;
+		count++;
+	}
+	if (index > count)
+		return (-1);
+
+	return (-1);
+}
